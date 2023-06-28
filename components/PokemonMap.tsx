7d1e99f30482cc49524245f3fc10855f93297598
Arek <arek.stachyra41@gmail.com>
2023-06-28 13:51:46 +0200

    feat: add initial marker functionality to map

--- components/PokemonMap.tsx
+++ components/PokemonMap.tsx
@@ -1,10 +1,34 @@
-import MapView from 'react-native-maps';
+import MapView, { LongPressEvent, Marker } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+
+type Marker = {
+	latitude: number;
+	longitude: number;
+};
+
+// I can probably replace index when mapping and add
+// a specific pokemon to the markers object
 
 export default function PokemonMap() {
+	const [markers, setMarkers] = useState<Marker[]>([]);
+
+	function handleLongPress(e: LongPressEvent) {
+		const markerCoords = e.nativeEvent.coordinate;
+		setMarkers([...markers, markerCoords]);
+	}
+
 	return (
 		<View style={styles.container}>
-			<MapView style={styles.map} />
+			<MapView style={styles.map} onLongPress={handleLongPress}>
+				{markers.map((marker, index) => (
+					<Marker
+						key={index}
+						coordinate={marker}
+						onPress={() => console.log('dupa')}
+					/>
+				))}
+			</MapView>
 		</View>
 	);
 }
