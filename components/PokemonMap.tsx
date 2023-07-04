1003d3082cba242602a5c1e39bba478bfa594bda
Arek <arek.stachyra41@gmail.com>
2023-07-04 16:48:46 +0200

    feat: add ability to pin locations on the map

--- components/PokemonMap.tsx
+++ components/PokemonMap.tsx
@@ -1,31 +1,89 @@
 import MapView, { LongPressEvent, Marker } from 'react-native-maps';
+import { v4 as uuidv4 } from 'uuid';
 import { StyleSheet, View } from 'react-native';
 import { useState } from 'react';
-
-type Marker = {
-	latitude: number;
-	longitude: number;
-};
-
-// I can probably replace index when mapping and add
-// a specific pokemon to the markers object
+import AddPinModal from './AddPinPinModal';
+import PinDetailsModal from './PinDetailsModal';
+import type { MapPin } from './components.types';
 
 export default function PokemonMap() {
-	const [markers, setMarkers] = useState<Marker[]>([]);
+	const [pins, setPins] = useState<MapPin[]>([]);
+	const [currentPin, setCurrentPin] = useState<MapPin | null>(null);
+
+	// Think about combining these two states into one object
+	const [isAddPinModalVisible, setIsAddPinModalVisible] = useState(false);
+	const [isPinDetailsModalVisible, setIsPinDetailsModalVisible] =
+		useState(false);
 
 	function handleLongPress(e: LongPressEvent) {
-		const markerCoords = e.nativeEvent.coordinate;
-		setMarkers([...markers, markerCoords]);
+		const { coordinate } = e.nativeEvent;
+
+		setIsAddPinModalVisible(true);
+		setCurrentPin({
+			id: uuidv4(),
+			name: '',
+			notes: '',
+			coords: { ...coordinate },
+		});
+	}
+
+	function handleCloseModal() {
+		setIsAddPinModalVisible(false);
+		setIsPinDetailsModalVisible(false);
+	}
+
+	function handlePinPress(pinId: string) {
+		const pin = pins.find((pin) => pin.id === pinId);
+		if (!pin) {
+			throw new Error('Pin not found!');
+		}
+		setCurrentPin(pin);
+		setIsPinDetailsModalVisible(true);
+	}
+
+	function handleAddPin(name: string, notes: string) {
+		if (!currentPin) {
+			throw new Error('Current marker is not set!');
+		}
+
+		const updatedPin = {
+			...currentPin,
+			name: name,
+			notes: notes,
+			coords: { ...currentPin.coords },
+		};
+
+		setPins([...pins, updatedPin]);
+		setIsAddPinModalVisible(false);
+	}
+
+	function handleRemovePin(pinId: string) {
+		const updatedPins = pins.filter((pin) => pin.id !== pinId);
+		setPins(updatedPins);
+		setIsPinDetailsModalVisible(false);
 	}
 
 	return (
 		<View style={styles.container}>
+			<AddPinModal
+				isVisible={isAddPinModalVisible}
+				onClose={handleCloseModal}
+				title="Add pin for found pokemon"
+				onAddPin={handleAddPin}
+			/>
+			<PinDetailsModal
+				isVisible={isPinDetailsModalVisible}
+				onClose={handleCloseModal}
+				title="Pin details"
+				pinData={currentPin}
+				onRemovePin={handleRemovePin}
+			/>
 			<MapView style={styles.map} onLongPress={handleLongPress}>
-				{markers.map((marker, index) => (
+				{pins.map((pin) => (
 					<Marker
-						key={index}
-						coordinate={marker}
-						onPress={() => console.log('dupa')}
+						key={pin.id}
+						coordinate={pin.coords}
+						onPress={() => handlePinPress(pin.id)}
 					/>
 				))}
 			</MapView>
